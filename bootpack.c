c704d900a64ea57e31a604a65ac349dd4b52bafc
Jianqiu Zhang <zhangjianqiu_133@yeah.net>
2015-06-11 11:56:32 +0800

    Palette now set up

--- bootpack.c
+++ bootpack.c
@@ -7,14 +7,25 @@
  ************************************************************************/
 
 void io_hlt(void);
-void write_mem8(int addr, int data);
+void io_cli(void);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+
+void init_palette(void);
+void set_palette(int start, int end, unsigned char* rgb);
 
 void HariMain(void)
 {
 	int i;
+	char* p;		// BYTE PTR
+
+	init_palette();	/* setup the palette */
+
 	for(i = 0x0a0000; i <= 0x0affff; i++)
 	{
-		write_mem8(i, i%16);
+		p = i;
+		*p = i%16;
 	}
 	for(;;)
 	{
@@ -22,3 +33,45 @@ void HariMain(void)
 	}
 }
 
+void init_palette(void)
+{
+	/* 定义颜色表 */
+	/* static char 就相当于汇编的 DB */
+	static unsigned char table_rgb[16*3] = {
+		0x00,0x00,0x00,		//black
+		0xff,0x00,0x00,		//red
+		0x00,0xff,0x00,		//green
+		0xff,0xff,0x00,		//yellow
+		0x00,0x00,0xff,		//blue
+		0xff,0x00,0xff,		//purple
+		0x00,0xff,0xff,		//light blue
+		0xff,0xff,0xff,		//white
+		0xc6,0xc6,0xc6,		//grey
+		0x84,0x00,0x00,		//dark red
+		0x00,0x84,0x00,		//dark green
+		0x84,0x84,0x00,		//dark yellow
+		0x00,0x00,0x84,		//dark blue
+		0x84,0x00,0x84,		//dark purple
+		0x00,0x84,0x84,		//light dark blue
+		0x84,0x84,0x84		//dark grey
+	};
+	set_palette(0, 15, table_rgb);
+	return ;
+}
+
+void set_palette(int start, int end, unsigned char *rgb)
+{
+	int i,eflags;
+	eflags = io_load_eflags();
+	io_cli();
+	io_out8(0x03c8, start);
+	for( i = start; i <= end; i++)
+	{
+		io_out8(0x03c9, rgb[0] / 4);
+		io_out8(0x03c9, rgb[1] / 4);
+		io_out8(0x03c9, rgb[2] / 4);
+		rgb += 3;
+	}
+	io_store_eflags(eflags);
+	return ;
+}
--- naskfunc.nas
+++ naskfunc.nas
@@ -7,7 +7,10 @@
 
 [FILE "naskfunc.nas"]
 	
-	GLOBAL	_io_hlt,_write_mem8
+	GLOBAL	_io_hlt, _io_cli, _io_sti, _io_stihlt
+	GLOBAL	_io_in8, _io_in16, _io_in32
+	GLOBAL	_io_out8, _io_out16, _io_out32
+	GLOBAL	_io_load_eflags, _io_store_eflags
 
 [SECTION .text]			;必须写
 
@@ -15,11 +18,63 @@ _io_hlt:	;void io_hlt(void);
 	HLT
 	RET
 
-_write_mem8:	;void write_mem8(int addr,int data);
-	;定义新函数用于向内存中写入数据
-	MOV		ECX,[ESP+4]	;ESP+4中存放的是addr的值
-	MOV		AL,[ESP+8]	;ESP+8中存放的是data的值
-	MOV		[ECX],AL
+_io_cli:	;void io_cli(void);
+	CLI
+	RET
+
+_io_sti:	;void io_sti
+	STI
+	RET
+
+_io_stihlt:	;void io_stihlt(void)
+	STI
+	HLT
+	RET
+
+_io_in8:	;int io_in8(int port);
+	MOV		EDX,[ESP+4]
+	MOV		EAX,0
+	IN		AL,DX
+	RET
+
+_io_in16:	;int io_in16(int port)
+	MOV		EDX,[ESP+4]
+	MOV		EAX,0
+	IN		AX,DX
+	RET
+
+_io_in32:	;int io_in32(int port)
+	MOV		EDX,[ESP+4]
+	IN		EAX,DX
+	RET
+
+_io_out8:	;void io_out8(int port, int data);
+	MOV		EDX,[ESP+4]
+	MOV		EAX,[ESP+8]
+	OUT		DX,AL
+	RET
+
+_io_out16:	;void io_out_16(int port, int data);
+	MOV		EDX,[ESP+4]
+	MOV		EAX,[ESP+8]
+	OUT		DX,AX
+	RET
+
+_io_out32:	;void io_out32(int port, int data);
+	MOV		EDX,[ESP+4]
+	MOV		EAX,[ESP+8]
+	OUT		DX,EAX
+	RET
+	
+_io_load_eflags:	;int void io_load_eflags(void);
+	PUSHFD
+	POP		EAX
+	RET
+
+_io_store_eflags:	;void io_store_eflags(int eflags);
+	MOV		EAX,[ESP+4]
+	PUSH	EAX
+	POPFD
 	RET
 
 
